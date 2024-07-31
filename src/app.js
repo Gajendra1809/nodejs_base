50dc00a2a0fd47ef83ab4a6bc991b3871f17dd04
Gajendra Chilhate <chilhategajendra@gmail.com>
2024-07-31 16:49:17 +0530

    changes in security

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import "dotenv/config"
 
 import userRouter from "./routes/user.routes.js";
 import adminRouter from "./routes/admin.routes.js";
--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://gajendra:Gfg12345@cluster0.objw3po.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/testNode")
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
         console.log("Connected to MongoDB");
     } catch (err) {
         console.log(err, "Database connection error");
